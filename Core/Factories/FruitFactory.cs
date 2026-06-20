c4a1565d6e6b8a61bea05dd94793b1f11b89ae0d
Denys <nesfow@gmail.com>
2026-06-20 20:15:45 +1000

    feat: adding Fruit factory (factory pattern) for fruit creation based on the fruit name

---
+++ Core/Factories/FruitFactory.cs
@@ -0,0 +1,21 @@
+using Core.Entities;
+using Core.Pricing;
+
+namespace Core.Factories;
+
+public class FruitFactory
+{
+    public static Fruit Create(string fruitName)
+    {
+        return fruitName.Trim().ToLowerInvariant() switch
+        {
+            "apple" => new Fruit("apple", 1m, new PerItemPricingStrategy()),
+            "banana" => new Fruit("banana", 1.5m, new PerItemPricingStrategy()),
+            "cherry" => new Fruit("cherry", 5m, new PerKgPricingStrategy()),
+            "mango" => new Fruit("mango", 3.99m, new PerItemPricingStrategy()),
+            "watermelon" => new Fruit("watermelon", 6.5m, new PerItemPricingStrategy()),
+
+            _ => throw new ArgumentException($"'{fruitName}' is not an available item to order.\n Fruits available: 'apple', 'banana', 'cherry', 'mango', 'watermelon'")
+        };
+    }
+}
