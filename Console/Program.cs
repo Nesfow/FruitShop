dd4eb59f9cda02060e352490d1e9739e8ce3aa2d
Denys <nesfow@gmail.com>
2026-06-20 20:32:56 +1000

    feat: creating and printing order to the console

--- Console/Program.cs
+++ Console/Program.cs
@@ -1,1 +1,19 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using Core.Entities;
+using Core.Factories;
+
+var order = new Order()
+    .AddLine(FruitFactory.Create("apple"), 5)
+    .AddLine(FruitFactory.Create("banana"), 11)
+    .AddLine(FruitFactory.Create("cherry"), 1)
+    .AddLine(FruitFactory.Create("mango"), 2);
+
+Console.WriteLine("🍎 Fruit Shop Order");
+Console.WriteLine("=================================================");
+
+foreach (var item in order.Lines)
+{
+    Console.WriteLine($"{item.Fruit.Name} x{item.Quantity} (${item.Fruit.BasePrice} {item.Fruit.PricingStrategy.Description}): ${item.LineCost}");
+
+}
+Console.WriteLine("=================================================");
+Console.WriteLine($"Total: ${order.Lines.Sum(x => x.LineCost)}");
