b7bf3101592007792f1c7a9e09086184be3192a7
Denys <nesfow@gmail.com>
2026-06-21 00:02:11 +1000

    fix: fixing wrong discount pricing strategy for cherry - utilizing per kg as internal strategy instead of per item

--- Core/Factories/FruitFactory.cs
+++ Core/Factories/FruitFactory.cs
@@ -9,7 +9,7 @@ public class FruitFactory
     {
         "apple" => new Fruit("Apple", 2m, new PerKgPricingStrategy()),
         "banana" => new Fruit("Banana", 0.3m, new PerItemPricingStrategy()),
-        "cherry" => new Fruit("Cherry", 5m, new DiscountPricingStrategy(new PerItemPricingStrategy(), 2, 0.1m)),
+        "cherry" => new Fruit("Cherry", 5m, new DiscountPricingStrategy(new PerKgPricingStrategy(), 2, 0.1m)),
         "mango" => new Fruit("Mango", 3.99m, new PerItemPricingStrategy()),
         "watermelon" => new Fruit("Watermelon", 6.5m, new PerItemPricingStrategy()),
 
