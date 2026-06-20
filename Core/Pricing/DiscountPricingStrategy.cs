19156c3fce15c32823e16235a38b2cb5aa3965e6
Denys <nesfow@gmail.com>
2026-06-20 20:27:06 +1000

    feat: adding description to the discount pricing strategy

--- Core/Pricing/DiscountPricingStrategy.cs
+++ Core/Pricing/DiscountPricingStrategy.cs
@@ -6,7 +6,7 @@ public class DiscountPricingStrategy : IPricingStrategy
 {
     private readonly IPricingStrategy _innerStrategy;
     private readonly decimal _quantityThreshold;
-    private readonly decimal _discountPercentage;
+    private readonly decimal _discountPercentage; // in decimal value: e.g. 0.1 = 10%
 
     public DiscountPricingStrategy(IPricingStrategy innerStrategy, decimal quantityThreshold, decimal discountPercentage)
     {
@@ -15,7 +15,7 @@ public class DiscountPricingStrategy : IPricingStrategy
         _innerStrategy = innerStrategy;
     }
 
-    public string Description => throw new NotImplementedException();
+    public string Description => $"{_innerStrategy.Description} with {_discountPercentage:P0} off over {_quantityThreshold}";
 
     public decimal Calculate(decimal basePrice, decimal quantity)
     {
